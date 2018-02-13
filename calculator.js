29d255abc77081b5e85b16115bafb6abc108011f
Steve Torrens <stevetorrens@Steves-MacBook-Air.local>
2018-02-13 21:57:43 +1300

    Added AC and CE button function

--- calculator.js
+++ calculator.js
@@ -1,20 +1,29 @@
 
+//Calculator
+
+// Set entries to an empty array. Each element represents a part of the numerical expression to be calculated
+// Set temp to an empty string. This variable holds a partially entered number.
+
 var entries = []
 var temp = ''
-
 
 document.addEventListener('DOMContentLoaded', start)
 
 function start () {
     
-  var items = document.getElementsByClassName('number')
-  for (var i = 0; i < items.length; i++) {
+  var numberButtons = document.getElementsByClassName('number')
+  for (var i = 0; i < numberButtons.length; i++) {
     console.log("hello1")
-    items[i].addEventListener('click',processNumber)
+    numberButtons[i].addEventListener('click',processNumber)
   }  
 
-    
-    
+  var ctrlButtons = document.getElementsByClassName('ctrl')
+  for (var i = 0; i < ctrlButtons.length; i++) {
+    console.log("hello2")
+    ctrlButtons[i].addEventListener('click',processCtrl)
+  }  
+
+
 }
 
 // If the button is a number or a “.” then
@@ -24,8 +33,27 @@ function start () {
 
 function processNumber (evt) {
   var calculatorDisplay = document.getElementsByClassName('display')[0]
-  
   temp += evt.target.innerText    
   calculatorDisplay.innerHTML = temp
   
 }
+
+// Else if the button is “AC” then 
+// 	Reset the entries to an empty array and temp to an empty string
+// 	Clear the calculator display
+
+// Else if the button is “CC” then 
+// 	Reset temp to an empty string 
+// 	(Retain the entries array - this is “stored” in the calculators memory)
+// 	Clear the calculator display
+
+function processCtrl (evt) {
+  var calculatorDisplay = document.getElementsByClassName('display')[0] 
+  var ctrl = evt.target.innerText
+  if (ctrl = "AC") {
+    entries = []
+    temp = ''
+  } else temp = ''
+  calculatorDisplay.innerHTML = temp
+}
+
